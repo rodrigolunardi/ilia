37148a967720f4ffda37c5327f81326d2047ee03
rodrigolunardi <rodrigolunardi14@gmail.com>
2021-05-23 18:53:39 -0300

    initial commit

---
+++ src/main/java/com/rodrigolunardi/ilia/IliaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigolunardi.ilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IliaApplication.class, args);
+	}
+
+}
